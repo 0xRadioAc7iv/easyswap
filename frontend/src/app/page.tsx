fb7c9319885f88a25a49ae404e054599514f4927
Manav <manav18gadhiya@gmail.com>
2024-12-08 02:15:24 +0530

    add: anon aadhaar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,32 @@
 "use client";
 
 import { Page } from "@/components/Page";
+import {
+  LogInWithAnonAadhaar,
+  useAnonAadhaar,
+  useProver,
+} from "@anon-aadhaar/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <Page back={false}>Hello!</Page>;
+  const [anonAadhar] = useAnonAadhaar();
+  const [, latestProof] = useProver();
+  const [isUserVerified, setUserVerified] = useState(false);
+  const [connectButtondisabled, setConnectButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if (anonAadhar.status === "logged-in") {
+      setUserVerified(true);
+    }
+  }, [anonAadhar]);
+
+  return (
+    <div>
+      <div>
+        <LogInWithAnonAadhaar nullifierSeed={1234} />
+        <div>Verify yourself with Anon Aadhar</div>
+      </div>
+      <div>{isUserVerified && <button>Connect Wallet</button>}</div>
+    </div>
+  );
 }
